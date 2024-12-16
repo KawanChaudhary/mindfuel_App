5041165668c3265aeb238868782e8d2b19b6c17b
kawan <kawanchaudhary@gmail.com>
2024-12-16 18:33:33 +0530

    fixed loader

--- src/Components/GeneralScreens/Loader.js
+++ src/Components/GeneralScreens/Loader.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import LottieView from 'lottie-react-native';
 
-const Loader = () => {
+const Loader = ({children, loading}) => {
+  const pathFile = require('../../../assets/loader/loader.json');
 
-    const pathFile = require('../../../assets/loader/loader.json');
+  if (!loading) {
+    return <View style={styles.loader}>{children}</View>;
+  }
 
   return (
-      <LottieView
-        source={pathFile}
-        autoPlay
-        loop
-        style={styles.container}
-      />
+    <LottieView source={pathFile} autoPlay loop style={styles.container} />
   );
 };
 
@@ -21,6 +19,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    transform: [{scale: 0.5}]
+  },
+  loader: {
+    flex: 1,
   },
 });
 
