323e8cb814897c4a408f1f969235e164eaef0744
kawan <kawanchaudhary@gmail.com>
2024-12-16 18:33:20 +0530

    fixed loader

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import RootNavigator from './src/Navigators/RootNavigator';
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import 'react-native-devsettings/withAsyncStorage';
 
 function App() {
   return <RootNavigator />;
